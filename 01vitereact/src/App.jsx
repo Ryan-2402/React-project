4897abe2afcb410211b91d07f4c95d9ffeb584fd
Ryan-2402 <syedryan2402@gmail.com>
2023-12-28 17:26:40 +0530

    create react and vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+ 
+
+  return (
+    <>
+     <Chai/>
+     <h2>heyyyyy</h2>
+     <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  )
+}
+
+export default App
